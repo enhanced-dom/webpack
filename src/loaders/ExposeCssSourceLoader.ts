1469ba338e5825bf5c21918a37d194a0c7ade1ae
Enhanced Dom <enhanced.dom@gmail.com>
2022-07-24 21:55:44 +0200

    inserting stylesheets with custom attribute

--- src/loaders/ExposeCssSourceLoader.ts
+++ src/loaders/ExposeCssSourceLoader.ts
@@ -1,5 +1,6 @@
 import styleLoader from 'style-loader'
 import { uniqueId } from 'lodash'
+import { STYLESHEET_ATTRIBUTE_NAME } from '@enhanced-dom/css/cjs'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 const loader = function () {}
@@ -12,10 +13,10 @@ loader.pitch = function pitch(remainingRequest: string) {
   const index = result.indexOf('var update = API(content, options)')
   if (index <= -1) return result
   const insertIndex = index - 1
-  const stylesheetName = uniqueId('stylesheet-')
+  const stylesheetName = uniqueId('stylesheet')
   const insertAttr = `
     options.attributes = options.attributes || {}
-    options.attributes.title = "${stylesheetName}";
+    options.attributes["${STYLESHEET_ATTRIBUTE_NAME}"] = "${stylesheetName}";
   `
   return result.slice(0, insertIndex) + insertAttr + result.slice(insertIndex) + `export const _stylesheetName = "${stylesheetName}";`
 }
